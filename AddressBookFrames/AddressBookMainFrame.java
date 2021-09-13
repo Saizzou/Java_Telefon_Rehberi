cb65acc6a705e3ad90c1205af46ab6155daee63f
Mikael Pikulski <65252447+Saizzou@users.noreply.github.com>
2021-09-13 11:54:48 +0200

    Update AddressBookMainFrame.java

--- AddressBookFrames/AddressBookMainFrame.java
+++ AddressBookFrames/AddressBookMainFrame.java
@@ -6,7 +6,12 @@ public class AddressBookMainFrame extends Frame {
     public AddressBookMainFrame(PersonPanel personPanel, Button newButton){
         super("Adress Kitabi");
         setBackground(Color.LIGHT_GRAY);
-        add(personPanel, BorderLayout.NORTH);
+        Panel dataPanel = new Panel(new GridLayout(1,3));
+        dataPanel.add(new Label("Isim: "));
+        dataPanel.add(new Label("Soyisim: "));
+        dataPanel.add(new Label("Telefon No: "));
+        add(dataPanel,BorderLayout.PAGE_START);
+        add(personPanel, BorderLayout.CENTER);
 
         Panel southPanel = new Panel();
         southPanel.add(newButton);
